114661fd4ff185cd4e5f9b30619d0cfe502b95be
vpauchet <113097148+vpauchet@users.noreply.github.com>
2022-12-16 15:22:23 +0100

    update

---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Projet Site</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="CSS/CSS.css">
+    </head>
+    <body>
+        <div class="titre">
+            Accueil
+        </div>
+        <div id="block1">
+            <div id="prenom">  
+                Valentin Pauchet
+            </div>
+            <div id="accroche1">
+                    Tous est possible quand on y croit
+            </div>
+            <img src="image/Gif-unscreen.gif" width=10% height=10% style="float:center;">
+        </div>
+        <div class="titre">
+        A Propos
+        </div>
+        <div id="block2">
+            <div id="accroche2"> 
+                L'avenir appartient à ceux qui se lèvent tôt
+            </div>
+            <div id="apropos">
+                Je suis passionné d'informatique depuis la fin du collège , au début je ne savais pas trop dans quoi ; plus dans les jeux vidéos . 
+                Puis avec le temps le réseau ma plu de plus en plus et cela c'est confirmé avec mon Bac Pro Système Numérique option réseau . 
+                Un bac ne me suffisant pas , j'ai décidé de continuer en faisant un BTS SIO option SISR et pourquoi après une license.
+            </br>
+            Je n'ai jamais fait de site auparavant a part un projet en année de Terminal mais j'était plus sur la partie de trouver des idées ,
+             gérer la base de donnée et créer un logo pour le site que nous devions créer moi et mon groupe .
+            </br>
+            Après ce BTS voir la license après , je pense travailler dans une grosse société / boîte avec une grande infrastructure réseau , commme dans un hôpital par exemple .
+            D'ailleurs j'ai déjà fait plusieurs stages dans des lieux avec une grande infrastructure réseau.
+            </br>
+            </br>
+            <img src="image/baie.jpg" width=15% height=15% style="float:none;">
+            </div>
+        </div>
+    <div class="titre">
+            Compétences
+    </div>
+    <h1 class="text-center">Mes capacités dans l'informatique</h1>
+ <div class="container">
+     <p>HTML</p>
+     <div class="progressbar-wrapper">
+      <div title="downloading" class="progressbar HTML">60%</div>
+     </div>
+
+     <p>CSS</p>
+     <div class="progressbar-wrapper">
+      <div title="downloading" class="progressbar CSS">30%</div>
+     </div>
+
+     <p>PHP</p>
+     <div class="progressbar-wrapper">
+      <div title="downloaded" class="progressbar PHP">5%</div>
+     </div>
+
+<h1 class="text-center">Mes capacités en bureautique</h1>
+ <div class="container">
+
+     <p>Word</p>
+     <div class="progressbar-wrapper">
+      <div title="downloading" class="progressbar Word">80%</div>
+     </div>
+
+     <p>Excel</p>
+     <div class="progressbar-wrapper">
+      <div title="downloading" class="progressbar Excel">70%</div>
+     </div>
+ </div>
+</br>
+    <div class="titre">
+           Expériences
+    </div>      
+    <div id="experiences">
+        Flers Informatique Flers <br>
+    - Installations Windows 10 sur des Pc fixes <br>
+    - Vérifications de matériels informatiques <br>
+    </br>
+    Centre Hospitalier service informatique Flers: <br>
+    -Migration d’un domaine hospitalier vers un autre <br>
+    -Dépannages des équipements informatiques de l’hôpital <br>
+    </br>
+    MICLON Informatique St Georges Des Groseillers
+- Interventions chez des particuliers et des professionnelles pour des livraisons et/ou dépannages  <br>
+- Dépannages et installations de pc. <br>
+    </br>
+    <a href="pdf/CV.pdf">Lien CV </a>
+    </div>
+    <div class="titre">
+         Formation
+    </div>
+    <div id="formation">
+        Je suis en Bts SIO Alternant , spe SISR</br>
+        Je suis au lycée Sainte Ursule à Caen</br>
+        Ma formation est sur 2 ans  , de 2022 à 2024</br>
+        Le BTS SIO a pour objectif de former des techniciens qui participent à la production et à la fourniture de services informatiques à une organisation. </br>
+        <a href="pdf/CV.pdf">Lien CV </a>
+    </div>
+    <div class="titre">
+        Contact
+   </div>
+   <div id="contact">
+   Pauchet Valentin</br>
+   adresse.bidon@gmail.com</br>
+   Projet Site</br>
+   Présentation du site
+   </div>
+    </body>
+</html>
+<!-- ( Je n'ai pas mis de Yaml , car je ne vois pas ce que je peux mettre en yaml) -->
